ef9a17d32e156b76cc76fd4e5561aabaf8004938
Michael Lavers <kolanos@gmail.com>
2021-01-11 17:10:44 -0800

    Add Terraform Examples

---
+++ examples/terraform/nodejs/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const newrelic = require('newrelic');
+
+// In a Node.js Lambda, the runtime loads the handler code as a module; so code in the top level
+// of the module occurs once, during cold start.
+console.log("Lambda Handler starting up");
+
+exports.lambdaHandler = async (event, context) => {
+    // At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
+
+    // This is an example of a custom event. `FROM MyNodeEvent SELECT *` in New Relic will find this event.
+    newrelic.recordCustomEvent("MyNodeEvent", {
+        "zip": "zap"
+    });
+
+    // This attribute gets added to the normal AwsLambdaInvocation event
+    newrelic.addCustomAttributes({
+        "customAttribute": "customAttributeValue"
+    });
+
+    // As normal, anything you write to stdout ends up in CloudWatch
+    console.log("Hello, world");
+
+    return "Success!";
+};

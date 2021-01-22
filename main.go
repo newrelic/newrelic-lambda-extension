28aa0fd0b78ffec714f45517380bb8e6a9aad4a4
mrickard <maurice@mauricerickard.com>
2021-01-22 13:58:18 -0500

    fix: Expanding timeout detection window to 500ms
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- main.go
+++ main.go
@@ -191,7 +191,7 @@ func mainLoop(invocationClient *client.InvocationClient, batch *telemetry.Batch,
 		timeoutInstant := time.Unix(0, event.DeadlineMs*int64(time.Millisecond))
 
 		// Set the timeout timer for a smidge before the actual timeout; we can recover from early.
-		timeoutWatchBegins := time.Millisecond * 15
+		timeoutWatchBegins := time.Millisecond * 500
 		timeout := time.NewTimer(timeoutInstant.Sub(time.Now()) - timeoutWatchBegins)
 		select {
 		case telemetryBytes := <-telemetryChan:

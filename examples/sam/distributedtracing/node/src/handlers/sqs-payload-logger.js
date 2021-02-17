446ee333ce79ec7b6706449986885643f5327aa0
Matt Whelan <mwhelan@newrelic.com>
2021-02-17 11:21:14 -0800

    DT wired up
    The Python Agent is producing tuples, while the Node Agent expects an
    Object. Find a cleaner way to resolve this.

--- examples/sam/distributedtracing/node/src/handlers/sqs-payload-logger.js
+++ examples/sam/distributedtracing/node/src/handlers/sqs-payload-logger.js
@@ -1,6 +1,19 @@
+'use strict';
+const newrelic = require('newrelic');
+
 /**
  * A Lambda function that logs the payload received from SQS.
  */
 exports.sqsPayloadLoggerHandler = async (event, context) => {
-    console.info(JSON.stringify(event));
+    event.Records.forEach(r => {
+        console.info(JSON.stringify(event));
+        if (r.messageAttributes.NRDT) {
+            let nrdtHeaders = JSON.parse(r.messageAttributes.NRDT.stringValue);
+            console.info("NRDT headers: %s", JSON.stringify(nrdtHeaders));
+
+            nrdtHeaders = nrdtHeaders.reduce((o, p) => { o[p[0]] = p[1]; return o;}, {});
+            console.info("NRDT headers processed: %s", JSON.stringify(nrdtHeaders));
+            newrelic.getTransaction().acceptDistributedTraceHeaders("Queue", nrdtHeaders);
+        }
+    })
 }
--- examples/sam/distributedtracing/python/rest_handler/app.py
+++ examples/sam/distributedtracing/python/rest_handler/app.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import os
+import newrelic
 
 QUEUE_URL = os.environ.get('QUEUE_URL')
 
@@ -8,9 +9,18 @@ def lambda_handler(event, context):
 
     sqs = boto3.client("sqs")
 
+    dt_headers = []
+    newrelic.agent.insert_distributed_trace_headers(headers=dt_headers)
+
     message_data = sqs.send_message(
         QueueUrl=QUEUE_URL,
         MessageBody="Hello",
+        MessageAttributes={
+            "NRDT": {
+                'DataType': 'String',
+                'StringValue': json.dumps(dt_headers)
+            }
+        }
     )
 
     print("Sent message id %s to queue URL %s" % (message_data["MessageId"], QUEUE_URL))

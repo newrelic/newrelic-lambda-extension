9a436404aebb418d73ad83699e42f6f4dc550212
kwest <kwest@newrelic.com>
2021-05-06 15:08:07 -0700

    Java layer example update

--- examples/sam/java/NewRelicExampleJava/src/main/java/com.newrelic.lambda.example/App.java
+++ examples/sam/java/NewRelicExampleJava/src/main/java/com.newrelic.lambda.example/App.java
@@ -2,8 +2,6 @@ package com.newrelic.lambda.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.newrelic.opentracing.LambdaTracer;
-import com.newrelic.opentracing.aws.LambdaTracing;
 import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
@@ -11,20 +9,9 @@ import io.opentracing.util.GlobalTracer;
 
 import java.util.Map;
 
-/**
- * Handler for requests to Lambda function.
- */
 public class App implements RequestHandler<Map<String, Object>, String> {
-    static {
-        // Register the New Relic OpenTracing LambdaTracer as the Global Tracer
-        GlobalTracer.registerIfAbsent(LambdaTracer.INSTANCE);
-    }
 
     public String handleRequest(final Map<String, Object> input, final Context context) {
-        return LambdaTracing.instrument(input, context, (event, ctx) -> handleInvocation());
-    }
-
-    private String handleInvocation() {
         final Tracer tracer = GlobalTracer.get();
 
         // This is an example of a custom span. `FROM Span SELECT * WHERE name='MyJavaSpan'` in New Relic will find this event.

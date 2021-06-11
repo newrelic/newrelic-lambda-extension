7b6a0b6f768be72c8cc2f06c09948d5a433db37c
Michael Lavers <kolanos@gmail.com>
2021-06-10 17:50:52 -0700

    Add Test For Named Pipe That Never Writes (#76)

--- main_test.go
+++ main_test.go
@@ -645,6 +645,106 @@ func TestMainTimeoutUnreachable(t *testing.T) {
 	assert.NotPanics(t, main)
 }
 
+func TestMainTimeoutNoPipeWrite(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(200*time.Millisecond))
+	defer cancel()
+	overrideContext(ctx)
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer util.Close(r.Body)
+
+		if r.URL.Path == "/2020-01-01/extension/register" {
+			w.Header().Add(api.ExtensionIdHeader, "test-ext-id")
+			w.WriteHeader(200)
+			res, err := json.Marshal(api.RegistrationResponse{
+				FunctionName:    "foobar",
+				FunctionVersion: "$latest",
+				Handler:         "lambda.handler",
+			})
+			assert.Nil(t, err)
+			_, _ = w.Write(res)
+		}
+
+		if r.URL.Path == "/2020-01-01/extension/init/error" {
+			w.WriteHeader(200)
+			_, _ = w.Write([]byte(""))
+		}
+
+		if r.URL.Path == "/2020-01-01/extension/exit/error" {
+			w.WriteHeader(200)
+			_, _ = w.Write(nil)
+		}
+
+		if r.URL.Path == "/2020-08-15/logs" {
+			w.WriteHeader(200)
+			_, _ = w.Write(nil)
+		}
+
+		if r.URL.Path == "/2020-01-01/extension/event/next" {
+			time.Sleep(25 * time.Millisecond)
+
+			w.WriteHeader(200)
+			res, err := json.Marshal(api.InvocationEvent{
+				EventType:          api.Invoke,
+				DeadlineMs:         100,
+				RequestID:          "12345",
+				InvokedFunctionARN: "arn:aws:lambda:us-east-1:12345:foobar",
+				ShutdownReason:     "",
+				Tracing:            nil,
+			})
+			assert.Nil(t, err)
+			_, _ = w.Write(res)
+		}
+	}))
+	defer srv.Close()
+
+	url := srv.URL[7:]
+
+	_ = os.Setenv(api.LambdaHostPortEnvVar, url)
+	defer os.Unsetenv(api.LambdaHostPortEnvVar)
+
+	_ = os.Setenv("NEW_RELIC_LICENSE_KEY", "foobar")
+	defer os.Unsetenv("NEW_RELIC_LICENSE_KEY")
+
+	_ = os.Setenv("NEW_RELIC_LOG_SERVER_HOST", "localhost")
+	defer os.Unsetenv("NEW_RELIC_LOG_SERVER_HOST")
+
+	_ = os.Setenv("NEW_RELIC_EXTENSION_LOG_LEVEL", "DEBUG")
+	defer os.Unsetenv("NEW_RELIC_EXTENSION_LOG_LEVEL")
+
+	_ = os.Remove("/tmp/newrelic-telemetry")
+
+	go func() {
+		pipeOpened := false
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				if _, err := os.Stat("/tmp/newrelic-telemetry"); os.IsNotExist(err) {
+					if pipeOpened {
+						return
+					} else {
+						continue
+					}
+				} else {
+					pipeOpened = true
+				}
+
+				pipe, err := os.OpenFile("/tmp/newrelic-telemetry", os.O_WRONLY, 0)
+				assert.Nil(t, err)
+				defer pipe.Close()
+
+				time.Sleep(200 * time.Millisecond)
+				pipe.Close()
+			}
+		}
+	}()
+
+	assert.NotPanics(t, main)
+}
+
 func overrideContext(ctx context.Context) {
 	rootCtx = ctx
 }

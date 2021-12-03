0320998b70813321e44ff3324e1be4da0a9aebda
Michael Lavers <kolanos@gmail.com>
2021-12-03 13:55:11 -0800

    Format SAM Deploy Scripts, Remove Debugging (#98)
    * Format SAM Deploy Scripts, Remove Debugging
    
    * Wrap quotes around s3:// path

--- examples/sam/java/deploy.sh
+++ examples/sam/java/deploy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 accountId=$1
-
 region=$2
+
 echo "region set to ${region}"
 
 sam build --use-container
 
 bucket="newrelic-example-${region}-${accountId}"
 
-#aws s3 mb --region ${region} s3://${bucket}
+aws s3 mb --region "${region}" "s3://${bucket}"
 
-sam package --region ${region} --s3-bucket ${bucket} --output-template-file packaged.yaml
-sam deploy --region ${region} \
-  --template-file packaged.yaml \
-  --stack-name NewrelicExampleJava2 \
-  --capabilities CAPABILITY_IAM \
-  --parameter-overrides "NRAccountId=${accountId}"
+sam package --region "${region}" --s3-bucket "${bucket}" --output-template-file packaged.yaml
+
+sam deploy \
+	--region "${region}" \
+	--template-file packaged.yaml \
+	--stack-name NewrelicExampleJava \
+	--capabilities CAPABILITY_IAM \
+	--parameter-overrides "NRAccountId=${accountId}"

5f308ca507f0cc930fdca1effb2ccf9fb02104bd
Kavinraja Ganesan <62742678+Kavinraja-G@users.noreply.github.com>
2021-12-04 03:29:57 +0530

    Add required envs for lambda extensions in TF examples (#81)

--- examples/terraform/nodejs/main.tf
+++ examples/terraform/nodejs/main.tf
@@ -42,6 +42,10 @@ resource "aws_lambda_function" "newrelic_terraform_example_nodejs_function" {
       # For the instrumentation handler to invoke your real handler, we need this value
       NEW_RELIC_LAMBDA_HANDLER = var.lambda_function_handler
       NEW_RELIC_ACCOUNT_ID     = var.newrelic_account_id
+      # Enable NR Lambda extension if the telemetry data are ingested via lambda extension
+      NEW_RELIC_LAMBDA_EXTENSION_ENABLED = true
+      # Enable Distributed tracing for in-depth monitoring of transactions in lambda (Optional)
+      NEW_RELIC_DISTRIBUTED_TRACING_ENABLED = true
     }
   }
   # This layer includes the New Relic Lambda Extension, a sidecar process that sends telemetry,

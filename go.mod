760b80ddaecc1a90523bb69974c7ca674492ce85
Matt Whelan <mwhelan@newrelic.com>
2021-12-30 13:30:10 -0800

    Some additional span translation work

--- go.mod
+++ go.mod
@@ -5,18 +5,14 @@ go 1.14
 require (
 	github.com/aws/aws-lambda-go v1.19.1 // indirect
 	github.com/aws/aws-sdk-go v1.34.21
-	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/uuid v1.1.2
 	github.com/newrelic/go-agent/v3 v3.9.0
 	github.com/newrelic/go-agent/v3/integrations/nrlambda v1.2.0
 	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/otel v1.3.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.3.0
+	go.opentelemetry.io/otel/sdk v1.3.0
 	go.opentelemetry.io/otel/trace v1.3.0
-	golang.org/x/mod v0.4.1
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
-	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/mod v0.4.2
 	google.golang.org/genproto v0.0.0-20200910191746-8ad3c7ee2cd1 // indirect
-	google.golang.org/grpc v1.32.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
 )
--- telemetry/agentdata/otel.go
+++ telemetry/agentdata/otel.go
@@ -2,25 +2,97 @@ package agentdata
 
 import (
 	"context"
+	"fmt"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 	"time"
 )
 
+func foo(ctx context.Context, licenseKey string) trace.Tracer {
+	exporter, _ := otlptracegrpc.New(ctx,
+		otlptracegrpc.WithHeaders(map[string]string{"api-key": licenseKey}),
+		otlptracegrpc.WithEndpoint("https://otlp.nr-data.net:4317/"),
+	)
+
+	tp := tracesdk.NewTracerProvider(tracesdk.WithBatcher(exporter))
+	return tp.Tracer("newrelic-lambda-extension")
+}
+
 func ReplaySpans(ctx context.Context, nrSpans []AgentEvent, tracer trace.Tracer) {
 	childIndex, root := indexAgentEvents(nrSpans)
 
-	ReplaySpan(ctx, tracer, root, childIndex)
+	ReplaySpan(ctx, tracer, root, childIndex, nil, nil)
 }
 
-func ReplaySpan(ctx context.Context, tracer trace.Tracer, nrSpan *AgentEvent, index map[string][]*AgentEvent) {
+func ReplaySpan(
+	ctx context.Context,
+	tracer trace.Tracer,
+	nrSpan *AgentEvent,
+	index map[string][]*AgentEvent,
+	nrErrorEvents []*AgentEvent,
+	nrCustomEvents []*AgentEvent,
+) {
 	ts := ToTimestamp(nrSpan.Get("timestamp").(float64))
-	attrs := nrSpanAttributes(nrSpan)
-	spanCtx, span := tracer.Start(ctx, nrSpan.Get("name").(string), trace.WithTimestamp(ts), trace.WithAttributes(attrs...))
+	entryPoint := nrSpan.Get("nr.entryPoint")
+	spanKind := trace.SpanKindInternal
+	if entryPoint != nil && entryPoint.(bool) {
+		spanKind = trace.SpanKindServer
+	} else {
+		kindStr := nrSpan.Get("span.kind")
+		if kindStr != nil && kindStr.(string) == "client" {
+			spanKind = trace.SpanKindClient
+		}
+	}
+	attrs := agentEventAttributes(nrSpan)
+	spanCtx, span := tracer.Start(
+		ctx,
+		nrSpan.Get("name").(string),
+		trace.WithTimestamp(ts),
+		trace.WithAttributes(attrs...),
+		trace.WithSpanKind(spanKind),
+	)
+
+	//Errors
+	for _, ev := range nrErrorEvents {
+		eventTs := ToTimestamp(ev.Get("timestamp").(float64))
+		errorClass := ev.Get("error.class")
+		errorMessage := ev.Get("error.message")
+
+		eventAttrs := agentEventAttributes(ev)
+		if errorClass != nil {
+			eventAttrs = append(eventAttrs, attribute.String("exception.type", errorClass.(string)))
+		}
+		if errorMessage != nil {
+			eventAttrs = append(eventAttrs, attribute.String("exception.message", errorMessage.(string)))
+		}
+
+		var err error
+		if errorClass != nil && errorMessage != nil {
+			err = fmt.Errorf("%v: %v", errorClass, errorMessage)
+		} else if errorClass != nil {
+			err = fmt.Errorf("%v", errorClass)
+		} else if errorMessage != nil {
+			err = fmt.Errorf("%v", errorMessage)
+		} else {
+			err = fmt.Errorf("an unknown error occurred")
+		}
+
+		span.RecordError(err, trace.WithTimestamp(eventTs), trace.WithAttributes(eventAttrs...))
+	}
+
+	//Custom events
+	for _, ev := range nrCustomEvents {
+		name := ev.Get("type").(string)
+		eventTs := ToTimestamp(ev.Get("timestamp").(float64))
+		eventAttrs := agentEventAttributes(ev)
+		span.AddEvent(name, trace.WithTimestamp(eventTs), trace.WithAttributes(eventAttrs...))
+	}
 
 	id := nrSpan.Get("guid").(string)
 	for _, child := range index[id] {
-		ReplaySpan(spanCtx, tracer, child, index)
+		ReplaySpan(spanCtx, tracer, child, index, nil, nil)
 	}
 
 	durationSeconds := nrSpan.Get("duration").(float64)
@@ -34,8 +106,8 @@ func indexAgentEvents(agentEvents []AgentEvent) (map[string][]*AgentEvent, *Agen
 
 	var root *AgentEvent
 	for _, ae := range agentEvents {
-		entryPoint := ae.Get("nr.entryPoint").(bool)
-		if entryPoint {
+		entryPoint := ae.Get("nr.entryPoint")
+		if entryPoint != nil && entryPoint.(bool) {
 			root = &ae
 		} else {
 			parentId := ae.Get("parentId").(string)
@@ -46,12 +118,26 @@ func indexAgentEvents(agentEvents []AgentEvent) (map[string][]*AgentEvent, *Agen
 	return ret, root
 }
 
-func nrSpanAttributes(nrSpan *AgentEvent) []attribute.KeyValue {
+// attrFilter is a set of attributes that should not be copied across to OTel spans
+var attrFilter = map[string]bool{
+	"timestamp": true,
+	"guid":      true,
+	"duration":  true,
+	"name":      true,
+	"parentId":  true,
+	"type":      true,
+	"sampled":   true,
+	"priority":  true,
+}
+
+func agentEventAttributes(nrSpan *AgentEvent) []attribute.KeyValue {
 	ret := make([]attribute.KeyValue, 0)
 	for k, v := range nrSpan.Flatten() {
-		attr := toAttribute(k, v)
-		if attr.Valid() {
-			ret = append(ret, attr)
+		if !attrFilter[k] {
+			attr := toAttribute(k, v)
+			if attr.Valid() {
+				ret = append(ret, attr)
+			}
 		}
 	}
 	return ret

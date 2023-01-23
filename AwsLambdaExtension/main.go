f8d8524b5fcbbc501f56e4b75c9421580ef657ef
Emilio Garcia <i.am.emilio@gmail.com>
2023-01-23 17:18:35 -0500

    deadlock fixed and logging finalized

--- AwsLambdaExtension/main.go
+++ AwsLambdaExtension/main.go
@@ -17,14 +17,11 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	collectAgentData bool
-
 	l = log.WithFields(log.Fields{"pkg": "main"})
 )
 
@@ -32,9 +29,8 @@ func main() {
 	// Handle User Configured Settings
 	conf := agentTelemetry.GetConfig()
 	log.SetLevel(conf.LogLevel)
-	collectAgentData = conf.CollectAgentData
 
-	l.Info("[main] Starting the Telemetry API extension")
+	l.Info("[main] Starting the New Relic Telemetry API extension")
 	ctx, cancel := context.WithCancel(context.Background())
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
@@ -72,16 +68,10 @@ func main() {
 	l.Debug("[main] Subscription success")
 	dispatcher := telemetryApi.NewDispatcher(extensionApiClient.GetFunctionName(), &conf, ctx, conf.TelemetryAPIBatchSize)
 
-	// Optional - set 	up new relic telemetry client
-	// Disable extract trace ID because it is bugged
-	batch := agentTelemetry.NewBatch(conf.AgentTelemetryBatchSize, false, conf.LogLevel)
-	telemetryClient := agentTelemetry.New(conf, batch, true)
-	telemetryChan, err := agentTelemetry.InitTelemetryChannel()
-	if err != nil {
-		l.Fatalf("telemetry pipe init failed: %v", err)
-	}
+	// Set up new relic agent telemetry client
+	agentDispatcher := agentTelemetry.NewDispatcher(conf)
 
-	l.Info("[main] New Relic Telemetry API Extension registered and subscribed for Lambda event streams succesfully")
+	l.Info("[main] New Relic Telemetry API Extension succesfully registered and subscribed")
 
 	// Will block until invoke or shutdown event is received or cancelled via the context.
 	for {
@@ -97,77 +87,24 @@ func main() {
 				l.Errorf("[main] Exiting. Error: %v", err)
 				return
 			}
+			l.Debugf("[main] Received event %+v", res)
 
-			// Fetches agent telemetry from channel then harvests only if ready
-			batch.AddInvocation(res.RequestID, time.Now())
-			dispatchAgentTelemetry(ctx, telemetryChan, batch, telemetryClient, res, false)
 			// Dispatching log events from previous invocations
+			agentDispatcher.AddRequest(res)
 			dispatcher.Dispatch(ctx, telemetryListener.LogEventsQueue, false)
-
-			l.Debug("[main] Received event")
+			agentDispatcher.Dispatch(ctx, res, false)
 
 			if res.EventType == extensionApi.Invoke {
-				handleInvoke(res)
+				l.Debug("[handleInvoke]")
+				// we no longer care about this but keep it here just in case
 			} else if res.EventType == extensionApi.Shutdown {
-				// Dispatch all remaining telemetry, handle shutdown
+				// force dispatch all remaining telemetry, handle shutdown
+				l.Debug("[handleShutdown]")
 				dispatcher.Dispatch(ctx, telemetryListener.LogEventsQueue, true)
-
-				// Close the batch and dump all remaining telemetry into harvest
-				// this harvest will be forced, dumping telemtry that may not be ready
-				dispatchAgentTelemetry(ctx, telemetryChan, batch, telemetryClient, res, true)
-				handleShutdown(res)
+				agentDispatcher.Dispatch(ctx, res, true)
+				l.Info("[main] New Relic Telemetry API Extension successfully shut down")
 				return
 			}
-		}
-	}
-}
-
-func handleInvoke(r *extensionApi.NextEventResponse) {
-	l.Info("[handleInvoke]")
-}
-
-func handleShutdown(r *extensionApi.NextEventResponse) {
-	l.Info("[handleShutdown]")
-}
-
-// Collect agent data and attempt to send it if appropriate
-// If force = true, collect and send data no matter what
-func dispatchAgentTelemetry(ctx context.Context, telemetryChan chan []byte, batch *agentTelemetry.Batch, telemetryClient *agentTelemetry.Client, res *extensionApi.NextEventResponse, force bool) {
-	if !collectAgentData {
-		return
-	}
-
-	// Fetch and Batch latest agent telemetry if possible
-	select {
-	case telemetryBytes := <-telemetryChan:
-		l.Debugf("[main] Got %d bytes of Agent Telemetry", len(telemetryBytes))
-		batch.AddTelemetry(res.RequestID, telemetryBytes)
-	default:
-	}
-
-	// Harvest and Send agent Data to New Relic
-	if force {
-		harvestAgentTelemetry(ctx, batch.Harvest(force), telemetryClient, res.InvokedFunctionArn)
-	} else {
-		if batch.ReadyToHarvest() {
-			harvestData := batch.Harvest(false)
-			harvestAgentTelemetry(ctx, harvestData, telemetryClient, res.InvokedFunctionArn)
-		}
-	}
-}
-
-// harvests and sends agent telemetry to New Relic
-func harvestAgentTelemetry(ctx context.Context, harvested []*agentTelemetry.Invocation, telemetryClient *agentTelemetry.Client, functionARN string) {
-	if len(harvested) > 0 {
-		l.Debugf("[main] sending agent harvest with %d invocations", len(harvested))
-		telemetrySlice := make([][]byte, 0, 2*len(harvested))
-		for _, inv := range harvested {
-			telemetrySlice = append(telemetrySlice, inv.Telemetry...)
-		}
-
-		numSuccessful, err := telemetryClient.SendTelemetry(ctx, functionARN, telemetrySlice)
-		if err != nil {
-			l.Errorf("[main] failed to send harvested telemetry for %d invocations %v", len(harvested)-numSuccessful, err)
 		}
 	}
 }

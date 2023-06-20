6bd24e2b85dea0bf9911b7fec66bba22a440753f
Leslie Alldridge <leslie.alldridge@gmail.com>
2023-06-21 06:46:47 +1200

    Update Terraform example to use python3.10

--- examples/terraform/python/main.tf
+++ examples/terraform/python/main.tf
@@ -4,8 +4,8 @@ module "python_test_function" {
   lambda_function_handler = "app.lambda_handler"
   wrapper_handler = "newrelic_lambda_wrapper.handler"
   lambda_function_name = "newrelic-terraform-example-python"
-  lambda_runtime = "python3.8"
+  lambda_runtime = "python3.10"
   lambda_zip_filename = "function.zip"
   newrelic_account_id = var.newrelic_account_id
-  newrelic_layer = "arn:aws:lambda:${var.aws_region}:451483290750:layer:NewRelicPython38:39"
+  newrelic_layer = "arn:aws:lambda:${var.aws_region}:451483290750:layer:NewRelicPython310:1"
 }

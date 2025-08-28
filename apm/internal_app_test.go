abf783dbb698a5e10d28fc4d6e5b9f840db342e0
Sashwatdas123 <Sashwatdas@newrelic.com>
2025-08-28 11:42:42 +0530

    chore: added Testcases for coverage

--- apm/internal_app_test.go
+++ apm/internal_app_test.go
@@ -17,4 +17,27 @@ func TestGetConnectBackoffTime(t *testing.T) {
 			t.Errorf("Invalid connect backoff for attempt #%d: got %v, want %v", k, b, v)
 		}
 	}
+	b := getConnectBackoffTime(10)
+	if b != 900*time.Millisecond {
+		t.Errorf("Out-of-bounds attempt should return last backoff, got %v", b)
+	}
+}
+func TestSetState(t *testing.T) {
+	app := &InternalAPMApp{}
+	run := &appRun{}
+	err := error(nil)
+	app.setState(run, err)
+	if app.Run != run {
+		t.Error("Run not set correctly")
+	}
+	if app.err != err {
+		t.Error("Error not set correctly")
+	}
+}
+
+func TestHarvestStruct(t *testing.T) {
+	h := &harvest{data: [][]byte{[]byte("test")}}
+	if len(h.data) != 1 {
+		t.Error("Expected harvest data length 1")
+	}
 }
